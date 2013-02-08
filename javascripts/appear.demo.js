e09f2f273d56f5d2dbbd1f1d351a3d86dc787974
Andrey Sidorov <takandar@gmail.com>
2013-02-08 21:14:47 +0400

    updated appear demo page

--- javascripts/appear.demo.js
+++ javascripts/appear.demo.js
@@ -1,15 +1,27 @@
 $(function() {
   var $appeared = $('#appeared');
-  $('section h3').appear(function($elements) {
-    $elements.yellowFade();
+  var $disappeared = $('#disappeared');
+
+  $('section h3').appear();
+  $('#force').on('click', function() {
+    $.force_appear();
+  });
+
+  $(document.body).on('appear', 'section h3', function(e, $affected) {
+    // this code is executed for each appeared element
+    $(this).yellowFade();
 
     $appeared.empty();
-    $elements.each(function() {
+    $affected.each(function() {
       $appeared.append(this.innerHTML+"\n");
     })
   });
 
-  $('#force').on('click', function() {
-    $.force_appear();
+  $(document.body).on('disappear', 'section h3', function(e, $affected) {
+    // this code is executed for each disappeared element
+    $disappeared.empty();
+    $affected.each(function() {
+      $disappeared.append(this.innerHTML+"\n");
+    })
   });
 });
